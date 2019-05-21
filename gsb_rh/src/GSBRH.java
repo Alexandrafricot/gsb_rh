5825946b4e41c02f43907b6950f5df6b837fd7f2
Alexandrafricot <37330519+Alexandrafricot@users.noreply.github.com>
2019-05-21 13:31:07 +0200

    Add files via upload

---
+++ gsb_rh/src/GSBRH.java
@@ -0,0 +1,22 @@
+import bzh.gsbrh.controleurs.Controleur;
+
+/**
+ * Classe d'entrée dans l'application
+ * 
+ * @author Alexandra Fricot
+ * @version 1.0
+ */
+public class GSBRH {
+
+	/**
+	 * Methode d'entrée dans l'application
+	 * 
+	 * @param args
+	 *            Passe un tableau d'argument a l'application
+	 */
+	public static void main(String[] args) {
+		Controleur controleur = new Controleur();
+		controleur.lancerAppli();
+	}
+
+}
